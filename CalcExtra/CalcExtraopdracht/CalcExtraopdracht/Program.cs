c022597190750276ecdd5720691d0f1d2c7a91bf
jan van foreest <30220@ma-web.nl>
2023-04-21 12:57:21 +0200

    flexklaarjeej

---
+++ CalcExtra/CalcExtraopdracht/CalcExtraopdracht/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace CalcExtraopdracht
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float x1, x2, y1, y2;
+            float rad1, rad2;
+            float testLength = 15;
+            x1 = 50;
+            y1 = 50;
+            x2 = 60;
+            y2 = 50;
+            rad1 = 5;
+            rad2 = 5;
+            float dx = x2 - x1;
+            float dy = y2 - y1;
+            testLength = 15;
+            double length = Math.Sqrt((dx * dx) + (dy + dy));
+            
+            bool collide = length <= testLength;
+            Console.WriteLine(collide);
+            
+        }
+    }
+}
---
+++ HelloWorld/HelloWorldopdracht/HelloWorldopdracht/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading;
+
+namespace HelloWorldopdracht
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string b = "Hello World!";
+            
+            foreach(char a in b)
+            {
+                
+                Console.Write(a);
+                Thread.Sleep(100);
+            }
+            Console.ReadLine();
+        }
+    }
+}
